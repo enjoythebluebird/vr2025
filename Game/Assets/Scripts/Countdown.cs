cf9a5d1bf0a735443719159c2907ad978fc6ccc3
joyjojosiwa <71533059+enjoythebluebird@users.noreply.github.com>
2025-02-27 05:36:07 -0600

    countdown fix
    im sleepy

--- Game/Assets/Scripts/Countdown.cs
+++ Game/Assets/Scripts/Countdown.cs
@@ -14,7 +14,13 @@ public class Countdown : MonoBehaviour
     {
         remainingTime -= Time.deltaTime;
         int minutes = Mathf.FloorToInt(remainingTime / 60);
-        int seconds = Mathf.FloorToInt(remainingime % 60);
-        timerText.text = string.Format("{0.00}:{1:00}", minutes, seconds);
+        int seconds = Mathf.FloorToInt(remainingTime % 60);
+        timerText.text = string.Format("{0:D2}:{1:D2}", minutes, seconds);
+
+        if (remainingTime <= 0)
+        {
+            Debug.Log("Time has run out, you're fired!");
+            Application.Quit();
+        }
     }
 }
