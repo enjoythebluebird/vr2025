e3f6b2cb5d3124059b6aa3e15d789dd533fa5ab7
joyjojosiwa <71533059+enjoythebluebird@users.noreply.github.com>
2025-03-25 09:55:25 -0500

    haptics

--- Game/Assets/Scripts/CustomHapticScript.cs
+++ Game/Assets/Scripts/CustomHapticScript.cs
@@ -1,11 +1,34 @@
+using System.Collections;
 using UnityEngine;
 
 public class CustomHapticScript : MonoBehaviour
 {
+    private bool picked = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
+    }
+
+    public void pickedup()
+    {
+        picked = true;
+    }
+
+    IEnumerator ContinuousVibration()
+    {
+        yield return new WaitForSeconds(1.0f);
+        GetComponent<HapticImpulsePlayer>().SendHapticFeedback(0.1f, 3f, 100f);
+        yield return new WaitForSeconds(3f);
+
+        if (picked){
+            StartCoroutine(ContinuousVibration());
+        }
+    }
+
+    public void pickedup()
+    {
+        picked = false;
     }
 
     public void lowamp()
