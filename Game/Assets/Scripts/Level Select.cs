08b5f7093931918810afca9a8b1dd3a9038a317e
eoe789 <eduardoo.enriquez789@gmail.com>
2025-03-27 03:58:11 -0500

    added haptics to level 1 and crated level select screen

---
+++ Game/Assets/Scripts/Level Select.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LevelSelect:MonoBehaviour
+{
+    public void Level1()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Level2()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Level3()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
